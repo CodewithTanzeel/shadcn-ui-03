7378eb4fb4e16715ebe80be7d41c09a08147cf7d
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-26 11:38:00 +0500

    update

---
+++ src/app/card.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+
+const CardDemo = () => {
+  return (
+    <div>
+        <Card>
+  <CardHeader>
+    <CardTitle>Card Title</CardTitle>
+    <CardDescription>Card Description</CardDescription>
+  </CardHeader>
+  <CardContent>
+    <p>Card Content</p>
+  </CardContent>
+  <CardFooter>
+    <p>Card Footer</p>
+  </CardFooter>
+</Card>
+
+
+    </div>
+  )
+}
+
+export default CardDemo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import CardDemo from "./card";
 
 export default function Home() {
   return (
    <div>
     <h1 className="text-center text-3xl pt-6">Shadcn UI Card Component</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+    <CardDemo />
+    </div>
    </div>
   );
 }

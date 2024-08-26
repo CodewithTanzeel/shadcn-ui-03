4d6738453d8f18b1c8160e07ebdc4d514c639620
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-26 11:57:17 +0500

    Update card.tsx

--- src/app/card.tsx
+++ src/app/card.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import {
     Card,
     CardContent,
@@ -12,17 +13,17 @@ import {
 
 const CardDemo = () => {
   return (
-    <div>
-        <Card>
+    <div className='w-screen h-screen flex justify-center p-10 items-center '>
+        <Card className='border-rounded bg-black text-white hover:bg-emerald-900'>
   <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
+    <CardTitle>14th-Gen Processors</CardTitle>
+    <CardDescription>By Intel</CardDescription>
   </CardHeader>
   <CardContent>
-    <p>Card Content</p>
+    <p>The latest Intel Core i7 CPU, featuring up to 12 cores and 5.0 GHz boost speeds, offers top-tier performance for gaming, content creation, and AI tasks. With support for PCIe 5.0, DDR5 memory, and advanced multi-threading, it's a powerhouse for demanding workloads.</p>
   </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
+  <CardFooter >
+    <Link href="https://www.intel.com/content/www/us/en/products/docs/processors/core/core-14th-gen-desktop-brief.html"  className='text-violet-700 hover:text-blue-700'>learn More</Link>
   </CardFooter>
 </Card>
 
